4e371aa47557224a342fe556ae3e00c8c75057d6
carlosasjr2003 <carlosasjr2003@hotmail.com>
2019-10-27 16:15:02 -0300

    Criacao das classes/Persistencia de Estado, Municipio e Hotel; Criacao da Tela de Login

--- Hotel/Hotel/Content/Master/Principal.master.cs
+++ Hotel/Hotel/Content/Master/Principal.master.cs
@@ -9,6 +9,10 @@ public partial class Content_Master_MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //Verifica se fez login, senão redireciona para a pagina de login,
+        /*  if (Convert.ToString(Session["LOGIN"]) == "")
+        {
+            Response.Redirect("../Paginas/login.aspx");
+        }*/
     }
 }
